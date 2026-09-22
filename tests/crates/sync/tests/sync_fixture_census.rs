8245785d37bbe53e5882e12235daf6da6eeaf688
Pavel Litvinenko <gerasim13@gmail.com>
2026-09-22 16:51:49 +0300

    test(warp): complete functional suite migration in ci

--- tests/crates/sync/tests/sync_fixture_census.rs
+++ tests/crates/sync/tests/sync_fixture_census.rs
@@ -4,8 +4,9 @@ use std::{panic::AssertUnwindSafe, path::Path};
 
 use futures::FutureExt;
 use kithara::platform::time::Duration;
-use kithara_integration_tests::{TestServerHelper, kithara};
+use kithara_integration_tests::TestServerHelper;
 use kithara_test_fixtures::assets::MANIFEST;
+use kithara_test_utils::kithara;
 
 use super::sync_product_matrix::{Provider, sources};
 
